b74d78e6853f87cc0a0adfec3663cb7ea65dd709
gabriellisboaconegero <gabriellisboaconegero@gmail.com>
2024-09-17 17:02:06 -0300

    Utilizando argparse para dar funcionalidade ao script

--- README.md
+++ README.md
@@ -17,5 +17,17 @@ conda create -n webScrap --file requirements.txt
 conda activate webScrap
 ```
 
+# Uso
+```shell
+usage: get.py [-h] [--dump-html]
+
+Faz um web scrapping
+
+options:
+  -h, --help   show this help message and exit
+  --dump-html  Se deve jogar o html da página pega
+```
+
+
 # Links úteis
 https://serpapi.com/blog/google-search-parameters/#:~:text=The%20'oq'%20parameter%20of%20Google,into%20the%20Google%20search%20box.
--- get.py
+++ get.py
@@ -2,30 +2,45 @@
 # ================== USE CONDA ==================
 from bs4 import BeautifulSoup
 import requests
+import argparse
 
-search = 'wikipedia grécia'
-url = 'https://www.google.com/search'
+parser = argparse.ArgumentParser(description='Faz um web scrapping')
+parser.add_argument(
+    '--dump-html',
+    dest='dump_html',
+    action='store_true',
+    help='Se deve jogar o html da página pega'
+)
+args = parser.parse_args()
 
-headers = {
+SEARCH = 'new rtx 5000 series'
+URL = 'https://www.google.com/search'
+
+HEADERS = {
 	'Accept' : '*/*',
-	'Accept-Language': 'en-US,en;q=0.5',
 	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82',
 }
-parameters = {'q': search}
+parameters = {'q': SEARCH}
 
-req1 = requests.get(url, headers = headers, params = parameters)
-# Sai se requesição não for 200 (OK)
-req1.raise_for_status()
-soup = BeautifulSoup(req1.text, 'html.parser')
+if __name__ == "__main__":
+    req1 = requests.get(URL, headers = HEADERS, params = parameters)
+    # Sai se requesição não for 200 (OK)
+    req1.raise_for_status()
+    soup = BeautifulSoup(req1.text, 'html.parser')
+    # Se deve dumpar a pagina html
+    if (args.dump_html):
+        print(soup)
+        exit(0)
 
-search = soup.find(id='search')
-links = search.find_all('a')
-# for link in links:
-#     print(link['href'])
+    search = soup.find(id='search')
+    links = search.find_all('a')
+    for link in links:
+        print(link['href'])
 
-req2 = requests.get(links[0]["href"])
-# Sai se requesição não for 200 (OK)
-req2.raise_for_status()
-soup = BeautifulSoup(req2.text, 'html.parser')
+    # # Pega html do primeiro item da pesquisa
+    # req2 = requests.get(links[0]["href"])
+    # # Sai se requesição não for 200 (OK)
+    # req2.raise_for_status()
+    # soup = BeautifulSoup(req2.text, 'html.parser')
 
-print(soup)
+    # print(soup)
