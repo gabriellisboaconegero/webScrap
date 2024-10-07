056c9579286ea2603be8a7818762afe5edcf7178
gabriellisboaconegero <gabriellisboaconegero@gmail.com>
2024-10-07 17:39:44 -0300

    feat: keywords de pesquisa passadas por linha de comando

--- get.py
+++ get.py
@@ -69,12 +69,14 @@ addarg('-ps', '--pg-start', dest='pg_start', type=parser.nonnegative_int, defaul
 addarg('-pc', '--pg-count', dest='pg_num', type=parser.positive_int,
        default=10, metavar='N', help='Número de resultados máximos (padrão 10)'
 )
+addarg('search_keywords', metavar='KEYWORD', type=str, nargs='+',
+       help='Palavras chave para realizar a busca'
+)
 parsed_args = parser.parse_args()
 # ------------------------- Argumentos do script -------------------------
 
 
 # ------------------------- Configuração de pesquisa -------------------------
-SEARCH = 'google'
 URL = 'https://www.google.com/search'
 
 HEADERS = {
@@ -97,9 +99,11 @@ def debug_print(*args, **kwargs):
     print(*args, **kwargs)
     print('DEBUG: --------------------------')
 
+# ------------------------- Configurações de Debug -------------------------
+
 def build_parameters():
     param = {
-        'q': SEARCH,
+        'q': ' '.join(parsed_args.search_keywords),
         # Coloquei 'uk' para ter os mesmos resultados no browser e na saida do script
         'gl': 'uk',
     }
@@ -113,44 +117,52 @@ def build_parameters():
 
     debug_print(f'{param=}')
     return param
-# ------------------------- Configurações de Debug -------------------------
 
+class OrganicResults:
+    def __init__(self, title, url, site_name, date, details):
+        self.title = title
+        self.url = url
+        self.site_name = site_name
+        self.date = date
+        self.details = details
 
-def parse_organic_result(og_result):
-    # O atributo data-snhf ajuda a dizer qual parte dos ados estamos utilizando
-    # Para achar eu explorei o código html de retorno
-    info_p0 = og_result.select_one('div[data-snhf="0"]')
-    info_p1 = og_result.select_one('div[data-sncf="1"]')
+    def __repr__(self):
+        a  = f'Title: {self.title}\n'
+        a += f'\t{self.site_name=}\n'
+        a += f'\t{self.url=}\n'
+        a += f'\t{self.date=}\n'
+        a += f'\t{self.details=}'
+        return a
 
-    title = info_p0.find('h3').get_text()
-    url = info_p0.find('a')['href']
-    # Achei qual classes deve ser explorando o html retornado
-    site_name = info_p0.select_one('span.VuuXrf').get_text()
+    @staticmethod
+    def parse(html_og_result):
+        # O atributo data-snhf ajuda a dizer qual parte dos ados estamos utilizando
+        # Para achar eu explorei o código html de retorno
+        info_p0 = html_og_result.select_one('div[data-snhf="0"]')
+        info_p1 = html_og_result.select_one('div[data-sncf="1"]')
 
-    # Mesma coisa, explorei o html e achei esse caminho para pegar a data
-    date = info_p1.select_one('span.LEwnzc.Sqrs4e')
-    if date != None:
-        date = date.get_text()
+        title = info_p0.find('h3').get_text()
+        url = info_p0.find('a')['href']
+        # Achei qual classes deve ser explorando o html retornado
+        site_name = info_p0.select_one('span.VuuXrf').get_text()
 
-    # Mesma coisa, explorei o html e achei esse caminho para pegar os detalhes
-    # Precisa do 'div > ...', pois se não tiver ele pode pegar oustros spans
-    # dentro do span quem tem a data
-    details = info_p1.select_one('div > span:not(.LEwnzc.Sqrs4e)')
-    if details == None:
-        # Pode acontecer de o texto estar diretamente na div (caso do reddit)
-        details = info_p1.get_text()
-    else:
-        details = details.get_text()
+        # Mesma coisa, explorei o html e achei esse caminho para pegar a data
+        date = info_p1.select_one('span.LEwnzc.Sqrs4e')
+        if date != None:
+            date = date.get_text()
 
-    # debug_print(f'{title=}\n{url=}\n{site_name=}\n{date=}\n{details=}')
-    return {
-        'title': title,
-        'url': url,
-        'site_name': site_name,
-        'date': date,
-        'datails': details,
-    }
+        # Mesma coisa, explorei o html e achei esse caminho para pegar os detalhes
+        # Precisa do 'div > ...', pois se não tiver ele pode pegar oustros spans
+        # dentro do span quem tem a data
+        details = info_p1.select_one('div > span:not(.LEwnzc.Sqrs4e)')
+        if details == None:
+            # Pode acontecer de o texto estar diretamente na div (caso do reddit)
+            details = info_p1.get_text()
+        else:
+            details = details.get_text()
 
+        # debug_print(f'{title=}\n{url=}\n{site_name=}\n{date=}\n{details=}')
+        return OrganicResults(title, url, site_name, date, details)
 
 if __name__ == "__main__":
     parameters = build_parameters()
@@ -168,7 +180,7 @@ if __name__ == "__main__":
     #       extra, como vídeos e tals. Achei isso usando o googler(GoogleParser.parse)
     #       e explorando o html de retorno
     organic_searches = soup.select('#search div.g.Ww4FFb.vt6azd.tF2Cxc.asEBEc')
-    organic_results = [parse_organic_result(val) for val in organic_searches]
+    organic_results = [OrganicResults.parse(val) for val in organic_searches]
     for og_res in organic_results:
-        print(og_res['title'])
+        print(og_res, end='\n\n')
 
